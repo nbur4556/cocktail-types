2aebfe0d615f4606f9d83d9c8a5fad4db04fb158
nbur4556 <nbur4556@gmail.com>
2021-05-19 18:07:14 -0500

    feat: search for cocktails by name

--- src/utils/cocktailDbConnection.tsx
+++ src/utils/cocktailDbConnection.tsx
@@ -1,17 +1,25 @@
 import axios from "axios";
+import { createBuilderStatusReporter } from "typescript";
 
 const baseUrl: string = 'https://www.thecocktaildb.com/api/json/v1';
 const apiKey: string | undefined = process.env.REACT_APP_COCKTAILDB_API_KEY;
 
-interface Drink {
+interface IDrink {
     [key: string]: string
 }
 
 // Return a random cocktail
-const getRandomCocktail = (cb: (result: Drink) => void) => {
+const getRandomCocktail = (cb: (x: IDrink) => void): void => {
     axios.get(`${baseUrl}/${apiKey}/random.php`)
+        .then(result => cb(result.data.drinks[0]))
+        .catch(err => console.log(err));
+}
+
+// Search Cocktails By Name
+const searchCocktailByName = (searchTerm: string, cb: (x: IDrink[]) => void): void => {
+    axios.get(`${baseUrl}/${apiKey}/search.php?s=${searchTerm}`)
         .then(result => cb(result.data.drinks))
         .catch(err => console.log(err));
 }
 
-export { getRandomCocktail };
+export { getRandomCocktail, searchCocktailByName };
