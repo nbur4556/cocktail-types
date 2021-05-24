9c6fbb735e443eef3f47d267118991676eb549c3
nbur4556 <nbur4556@gmail.com>
2021-05-24 18:42:35 -0500

    feat: load and format drink data into state

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,18 @@ import { useState, useEffect } from 'react';
 import ContentCard from './components/ContentCard';
 
 // Utils
+import formatDrinkData, { IDrinkData } from './utils/formatDrinkData';
 import { getRandomCocktail } from './utils/cocktailDbConnection';
 
 const App: React.FC = () => {
 
+  const [drinkData, setDrinkData] = useState<IDrinkData>();
+
   useEffect(() => {
-    getRandomCocktail(result => {
-      console.log(result);
-    });
-  });
+    getRandomCocktail(result => setDrinkData(formatDrinkData(result.data.drinks[0])));
+  }, []);
+
+  console.log(drinkData)
 
   return <main>
     <ContentCard />
