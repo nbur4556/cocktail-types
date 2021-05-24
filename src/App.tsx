8f92c6be35dcac1fe6b2b8f10346b609b395b19b
nbur4556 <nbur4556@gmail.com>
2021-05-24 18:03:20 -0500

    feat: get random cocktail on application load

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
+import { useState, useEffect } from 'react';
+
 // Components
 import ContentCard from './components/ContentCard';
 
+// Utils
+import { getRandomCocktail } from './utils/cocktailDbConnection';
+
 const App: React.FC = () => {
+
+  useEffect(() => {
+    getRandomCocktail(result => {
+      console.log(result);
+    });
+  });
+
   return <main>
-    <p>Hello World</p>
     <ContentCard />
   </main>
 }
