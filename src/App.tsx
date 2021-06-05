4f3b5218454b81ca3adb779d5825b7b1e6982d53
nbur4556 <nbur4556@gmail.com>
2021-06-04 19:16:56 -0500

    feat: search for cocktail by name

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import SearchSlider from './components/SearchSlider';
 
 // Utils
 import formatDrinkData from './utils/formatDrinkData';
-import { getRandomCocktail } from './utils/cocktailDbConnection';
+import { getRandomCocktail, searchCocktail } from './utils/cocktailDbConnection';
 
 // Interfaces
 import { IDrinkData } from './interfaces';
@@ -15,15 +15,20 @@ const App: React.FC = (): JSX.Element => {
 
   const [drinkData, setDrinkData] = useState<IDrinkData>();
 
+  // Set a single random cocktail as drink data state
   const setRandomCocktail = (): void => {
     getRandomCocktail(result => setDrinkData(formatDrinkData(result.data.drinks[0])));
+  }
+
+  const setSearchedCocktail = (): void => {
+    searchCocktail('name', 'brandy', result => setDrinkData(formatDrinkData(result.data.drinks[0])));
   }
 
   useEffect(setRandomCocktail, []);
 
   return <main>
     {(drinkData) ? <ContentCard drinkData={drinkData} /> : null}
-    <SearchSlider setRandomCocktail={setRandomCocktail} />
+    <SearchSlider setRandomCocktail={setRandomCocktail} setSearchedCocktail={setSearchedCocktail} />
   </main>
 }
 
--- src/components/SearchSlider.tsx
+++ src/components/SearchSlider.tsx
@@ -3,11 +3,13 @@ import SearchSliderButton from './SearchSliderButton';
 
 // Interfaces
 interface ISliderProps {
-    setRandomCocktail: () => void
+    setRandomCocktail: () => void,
+    setSearchedCocktail: () => void
 }
 
 const SearchSlider: React.FC<ISliderProps> = (props) => {
     return <section>
+        <SearchSliderButton onClick={props.setSearchedCocktail}>Search</SearchSliderButton>
         <SearchSliderButton onClick={props.setRandomCocktail}>Random</SearchSliderButton>
     </section>
 }
